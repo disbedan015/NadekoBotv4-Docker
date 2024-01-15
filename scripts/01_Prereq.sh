82d9d51875f4257e39cb36e795e6a8f9158b314d
disbedan015 <disbedan015@gmail.com>
2024-01-15 01:53:23 -0500

    More progress on to Nadeko app isntall

--- scripts/01_Prereq.sh
+++ scripts/01_Prereq.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -e
 # Install dotnet
-apt update && apt install curl -y && curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl && apt update && apt install apt-transport-https dotnet-sdk-6.0 -y && apt install libopus0 opus-tools libopus-dev libsodium-dev wget redis-server python3-all python-is-python3 libpython3.10 ffmpeg libopus0 opus-tools libopus-dev libsodium-dev youtube-dl -y && apt install ffmpeg -y && chmod a+rx /usr/local/bin/youtube-dl
+apt update && apt install curl -y && curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl && apt install apt-transport-https dotnet-sdk-6.0 -y libopus0 opus-tools libopus-dev libsodium-dev wget redis-server python3-all python-is-python3 libpython3.10 ffmpeg -y && chmod a+rx /usr/local/bin/youtube-dl
