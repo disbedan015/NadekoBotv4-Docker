fb129197f14a62d3938f3e206525cb4dad19c128
disbedan015 <disbedan015@gmail.com>
2024-01-15 22:20:22 -0500

    removed linebreak that everything hates.

--- scripts/04_Run.sh
+++ scripts/04_Run.sh
@@ -1,2 +1,1 @@
-redis-server --daemonize yes && cd /nadeko/ && \
-while true; do /usr/bin/dotnet /nadeko/NadekoBot.dll ; done
+redis-server --daemonize yes && cd /nadeko/ && while true; do /usr/bin/dotnet /nadeko/NadekoBot.dll ; done
