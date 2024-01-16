2c29d71298f5dd298fb87e20121ea0141721218e
disbedan015 <disbedan015@gmail.com>
2024-01-15 23:03:34 -0500

    fix yt-dlp dependancy and added auto update on bot restart

--- scripts/01_Prereq.sh
+++ scripts/01_Prereq.sh
@@ -1,3 +1,1 @@
-#!/bin/bash -e
-# Install dotnet
-apt update && apt install curl -y && curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl && apt install apt-transport-https dotnet-sdk-6.0 -y libopus0 opus-tools libopus-dev libsodium-dev wget redis-server python3-all python-is-python3 libpython3.10 ffmpeg -y && chmod a+rx /usr/local/bin/youtube-dl
+apt update && apt install curl -y &&  apt install apt-transport-https yt-dlp dotnet-sdk-6.0 -y libopus0 opus-tools libopus-dev libsodium-dev wget redis-server python3-all python-is-python3 libpython3.10 ffmpeg -y # && curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o /usr/local/bin/yt-dlp && chmod a+rx /usr/local/bin/yt-dlp
